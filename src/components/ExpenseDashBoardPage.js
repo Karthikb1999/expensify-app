e44d8fa7fd766a3986616ba4228c86607956f90b
Karthik Balaji <karthikb26101999@gmail.com>
2021-01-21 09:55:00 +0530

    Setup summary for expenses in dashboard

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,13 +1,29 @@
 import React from "react"
 import ExpenseList from "../components/ExpenseList"
 import ExpenseListFilters from "../components/ExpenseListFilters";
+import ExpenseSummary from "../components/ExpenseSummary";
+import { connect } from "react-redux"
+import selectExpensesTotal from "../selectors/expenses-total"
+import getVisibleExpenses from "../selectors/expenses"
 
 
-const ExpenseDashBoardPage = () => (
+
+export const ExpenseDashBoardPage = ({ expenses }) => (
     <div>
+        <ExpenseSummary
+            expensesCount={expenses.length}
+            expensesTotal={selectExpensesTotal(expenses)}
+        />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
 )
 
-export default ExpenseDashBoardPage
+
+const mapStateToProps = (state) => ({
+    expenses: getVisibleExpenses(state.expenses, state.filters)
+})
+
+
+
+export default connect(mapStateToProps)(ExpenseDashBoardPage)
--- src/tests/components/ExpenseDashBoardPage.test.js
+++ src/tests/components/ExpenseDashBoardPage.test.js
@@ -1,9 +1,9 @@
-import ExpenseDashBoardPage from "../../components/ExpenseDashBoardPage"
+import { ExpenseDashBoardPage } from "../../components/ExpenseDashBoardPage"
 import React from "react"
 import { shallow } from "enzyme"
-
+import expenses from "../fixtures/expenses"
 
 test("should render Expense DashBoard Page", () => {
-    const wrapper = shallow(<ExpenseDashBoardPage />)
+    const wrapper = shallow(<ExpenseDashBoardPage expenses={expenses} />)
     expect(wrapper).toMatchSnapshot()
 })
